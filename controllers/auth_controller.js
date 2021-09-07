ea05a399dd09bf065d585fe30a5d565ee87c3848
Leo Cucinell <leocucinell@gmail.com>
2021-09-07 15:09:10 -0700

    stubbed auth routes

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,8 +1,19 @@
 /* SECTION: Modules */
 const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const { User } = require("../models");
 
 /* SECTION: Middleware */
 
 /* SECTION: routes */
+//GET register/login => account
+
+//POST register
+
+//POST login
+
+//GET logout
 
 /* SECTION: export the router */
+module.exports = router;

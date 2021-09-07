d3c10c785ddd60c7d2b85b89cb78bf63622f1dce
Leo Cucinell <leocucinell@gmail.com>
2021-09-07 14:52:21 -0700

    Created the location mongoose model

--- models/Location.js
+++ models/Location.js
@@ -3,8 +3,20 @@ const mongoose = require("mongoose");
 
 /* SECTION: User schema */
 const locationSchema = new mongoose.Schema({
-    
-}, {timestamps: true});
+    latitude: {
+        type: Number,
+        required: [true, "please add the latitude for the event"]
+    },
+    longitude: {
+        type: Number,
+        required: [true, "please add the longitude for the event"]
+    },
+    address: {
+        type: String,
+        required: [true, "please add an address for the event"]
+    },
+    //look into adding a title to the location for popular locations
+});
 
 /* SECTION: User model */
 const Location = mongoose.model(locationSchema);

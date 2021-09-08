3529d2f6462d6099eef9cebf1de86d8dd69f807f
Leo Cucinell <leocucinell@gmail.com>
2021-09-08 12:17:02 -0700

    added sessions to the server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 /*SECTION: external modules */
 const express = require("express");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 /*SECTION: internal modules */
 const { auth } = require("./controllers");
@@ -10,6 +13,18 @@ const app = express();
 /*SECTION: app configuration */
 const PORT = process.env.PORT || 4000;
 app.set("view engine", "ejs");
+
+app.use(
+    session({
+        store: MongoStore.create({ mongoUrl: process.env.MONGO_URI || 'mongodb://localhost:27017/projectly' }),
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7 * 2, // two weeks
+        },
+    })
+);
 
 /*SECTION: middleware */
 app.use(express.static(__dirname + '/public'));

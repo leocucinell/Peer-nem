b9ce12f0decccdae5f13af209a76ba539c083867
Leo Cucinell <leocucinell@gmail.com>
2021-09-09 15:10:13 -0700

    added correct path to protected profile page and made ejs template for create event page

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -3,9 +3,17 @@ const express = require("express");
 const router = express.Router()
 /* SECTION: Middleware */
 
-/* SECTION: routes */
+/* SECTION: routes -> /main */
 router.get("/", (req, res, next) => {
-    res.render("main/main");
-})
+    res.render("events/main");
+});
+
+
+//GET Profile page
+router.get("/profile", (req, res, next) => {
+    res.render("auth/profile");
+});
+
+
 /* SECTION: export the router */
 module.exports = router;
